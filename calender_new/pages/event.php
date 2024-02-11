a6f9c0d9639296fa4f81ae78684b9fce69f68335
unknown <eamocha@gmail.com>
2024-02-12 02:27:22 +0300

    initial

--- calender_new/pages/event.php
+++ calender_new/pages/event.php
@@ -205,8 +205,8 @@ if ($mode == 'edit' and !$refresh) { //show/edit event
 	$cid2 = $row['category2_id'];
 	$uid = $row['user_id'];
 	$mayEdit = ($privs > 2 or ($privs == 2 and $uid == $_SESSION['uid'])) ? true : false; //edit rights
-	$des = preg_replace_callback($rxULink,create_function('$m','return $m[1].($m[2]!=$m[3]?" [".$m[3]."] ":" ");'),$desHtml);
-	$remks = preg_replace_callback($rxULink,create_function('$m','return $m[1].($m[2]!=$m[3]?" [".$m[3]."] ":" ");'),$remksHtml);
+	$des = preg_replace_callback($rxULink,function($m){ return $m[1].($m[2]!=$m[3]?" [".$m[3]."] ":" ");},$desHtml);
+	$remks = preg_replace_callback($rxULink,function($m){ return $m[1].($m[2]!=$m[3]?" [".$m[3]."] ":" ");},$remksHtml);
 	$des = str_replace(array("<br>", "<br />"), "\r\n", $des); //replace <br> by newline
 	$remks = str_replace(array("<br>", "<br />"), "\r\n", $remks); //replace <br> by newline
 	$nml = ($row['not_mail']) ? $row["not_mail"] : $umail;
@@ -397,7 +397,7 @@ if (($mode == "add_exe" or $mode == "upd_exe") and !$eMsg) { //no errors
 
 		$q = "INSERT INTO [db]events VALUES (NULL,DEFAULT,'$titEsc','$desEsc',$cid,$cid2,'$venEsc',$uid,DEFAULT,$pri,DEFAULT,'$sdate','$edate',DEFAULT,'$stime','$etime',$r_t,$r_i,$r_p,$r_m,'$runtil',$not,'$nml','".$todayD."',DEFAULT,DEFAULT,'$iid','$remksEsc')";
 		$result = dbQuery($q); //add to events table
-		$eid = mysqli_insert_id(); //set id to new event
+		$eid = mysql_insert_id(); //set id to new event
 
 
 		//add driver details
@@ -453,7 +453,7 @@ if (($mode == "add_exe" or $mode == "upd_exe") and !$eMsg) { //no errors
 			$result = dbQuery("UPDATE [db]events SET x_dates='$xda', editor='$uname', m_date='$todayD' WHERE event_id=$eid"); //exclude date from series
 			$q = "INSERT INTO [db]events VALUES (NULL,DEFAULT,'$titEsc','$desEsc',$cid,$cid2,'$venEsc',$uid,'".$uname."',$pri,DEFAULT,'$sdate','$edate',DEFAULT,'$stime','$etime',$r_t,$r_i,$r_p,$r_m,'$runtil',$not,'$nml','".$ada."','".$todayD."',DEFAULT,$iid,'$remksEsc')";
 			$result = dbQuery($q); //add new event
-			$eid = mysqli_insert_id(); //set id to new event
+			$eid = mysql_insert_id(); //set id to new event
 			$editN = 0;
 			$cMsg .= $xx['evt_confirm_added'];
 
